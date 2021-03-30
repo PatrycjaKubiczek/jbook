98565c23bf954f2fba5b1abdbd8a58c4f424883e
patrycjakubiczek <p.kubiczek@gmail.com>
2021-03-30 23:12:42 +0200

    initial

---
+++ src/plugins/unpkg-path-plugin.ts
@@ -0,0 +1,63 @@
+import * as esbuild from "esbuild-wasm";
+import axios from 'axios';
+import localForage from 'localforage';
+const fileCache = localForage.createInstance({
+  name: "filecache",
+});
+
+export const unpkgPathPlugin = (inputCode: string) => {
+  return {
+    name: "unpkg-path-plugin",
+    setup(build: esbuild.PluginBuild) {
+      // figure out where the index.js is stored
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        console.log("onResolve", args);
+        if(args.path === 'index.js') {
+          return { path: args.path, namespace: "a" };
+        }
+        if(args.path.includes('./') || args.path.includes('../')){
+          return {
+            namespace: 'a',
+            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
+          }
+        }
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`
+        }
+      });
+
+      // attempt to load up the index.js file
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log("onLoad", args);
+
+        if (args.path === "index.js") {
+          return {
+            loader: "jsx",
+            contents: inputCode,
+          };
+        } 
+        // check if we already fetched this file
+        // and if it is in the cache
+        const cachedResults = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
+
+        if(cachedResults){
+          return cachedResults;
+        }
+
+        const { data, request } = await axios.get(args.path);
+        
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname
+        }
+
+        // store response in cache
+        await fileCache.setItem(args.path, result)
+        
+        return result;
+      });
+    },
+  };
+};
