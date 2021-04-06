8ded76abe7b8e113d223280a3a0520e35fb90c7c
patrycjakubiczek <p.kubiczek@gmail.com>
2021-04-06 21:17:04 +0200

    add monaco code editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,24 @@
-import * as esbuild from "esbuild-wasm";
-import { useState, useEffect, useRef } from "react";
-import ReactDOM from "react-dom";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-import { fetchPlugin } from "./plugins/fetch-plugin";
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
+
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor'
 
 const App = () => {
   const ref = useRef<any>();
-  const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
+  const iframe = useRef<any>();
+
+  const [input, setInput] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
-
   useEffect(() => {
     startService();
   }, []);
@@ -25,37 +28,63 @@ const App = () => {
       return;
     }
 
-    // transform = transpile
+    iframe.current.srcdoc = html;
 
     const result = await ref.current.build({
-      entryPoints: ["index.js"],
+      entryPoints: ['index.js'],
       bundle: true,
       write: false,
       plugins: [
-          unpkgPathPlugin(), 
-          fetchPlugin(input)
-      ],
+        unpkgPathPlugin(),
+        fetchPlugin(input)],
       define: {
-        "process.env.NODE_ENV": '"production"',
-        global: "window",
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
       },
     });
 
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data)
+            } catch(err) {
+               const root = document.querySelector('#root');
+               root.innerHTML = '<div style="color: red; font-family: monospace;"><h4>Runtime error</h4>' + err + '</div>'
+               console.error(err);
+            }
+          }, false)
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
+      <CodeEditor
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
+      />
+      <textarea value={input}></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        ref={iframe}
+        title="preview"
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
